b20786b9d523c44287b32dcbc22c53ece7d26e82
Eunjin Kwon <77440874+eunjinfizz@users.noreply.github.com>
2023-05-06 14:18:57 +0900

    Time: 49 ms (92.29%), Space: 41.7 MB (66.99%) - LeetHub

---
+++ Easy/0278-first-bad-version/0278-first-bad-version.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for isBadVersion()
+ * 
+ * @param {integer} version number
+ * @return {boolean} whether the version is bad
+ * isBadVersion = function(version) {
+ *     ...
+ * };
+ */
+
+/**
+ * @param {function} isBadVersion()
+ * @return {function}
+ */
+var solution = function(isBadVersion) {
+    /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+    return function(n) {
+        let start = 1;
+        let end = n;
+
+        while(end - start >= 1){
+            // let middle = Math.floor((start + end) / 2);
+            let middle = Math.floor(start + (end - start) / 2); // To avoid overflow!
+            
+            if(isBadVersion(middle)) end = middle;
+            else start = middle + 1;
+        }
+        return end;
+    };
+};
