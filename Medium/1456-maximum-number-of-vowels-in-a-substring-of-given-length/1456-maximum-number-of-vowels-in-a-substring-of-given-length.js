0657816d81bac62c924545d52621204d5b55463c
Eunjin Kwon <77440874+eunjinfizz@users.noreply.github.com>
2023-05-06 23:22:14 +0900

    Time: 86 ms (82.35%), Space: 45.5 MB (71.63%) - LeetHub

--- Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ Medium/1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -4,19 +4,20 @@
  * @return {number}
  */
 var maxVowels = (s, k) => {
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u'])
     let answer = 0;
 
     for(let i = 0; i < k; i++){
-        if(vowels.includes(s[i])) answer++;
+        if(vowels.has(s[i])) answer++;
     }
     
+    if(answer === k) return answer;
     let count = answer;
     
     for(let i = 1; i <= s.length - k; i++){
-        if(vowels.includes(s[i-1])) count--;
-        if(vowels.includes(s[i-1+k])) count++;
-        answer = Math.max(count, answer);
+        if(vowels.has(s[i-1])) count--;
+        if(vowels.has(s[i-1+k])) count++;
+        if(count > answer) answer = count;
         if(answer === k) break;
     }
     
