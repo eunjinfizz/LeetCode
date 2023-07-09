9f03aa1e4d02eebc874acc4bf657412647155f3e
Eunjin Kwon <77440874+eunjinfizz@users.noreply.github.com>
2023-07-09 14:53:55 +0900

    Time: 69 ms (89.29%), Space: 45.5 MB (64.07%) - LeetHub

---
+++ Easy/0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.js
@@ -0,0 +1,26 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node|null} root
+ * @return {number[]}
+ */
+var preorder = function(root) {
+    const answer = [];
+    
+    const traverse = (node) => {
+        if(!node) return;
+        answer.push(node.val);
+        for(child of node.children){
+            traverse(child);
+        }
+    }
+    
+    traverse(root);
+    return answer;
+};
